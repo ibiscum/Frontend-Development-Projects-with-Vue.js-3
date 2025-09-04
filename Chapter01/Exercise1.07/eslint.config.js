e3e6dc10566b39ccda2dd8bc37b95d837a8cdb26
ibiscum <ulf.luecking@gmail.com>
2025-09-04 23:25:05 +0200

    update

---
+++ Chapter01/Exercise1.07/eslint.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from "eslint/config";
+import pluginVue from "eslint-plugin-vue";
+import globals from "globals";
+
+export default defineConfig([
+  // add more generic rulesets here, such as:
+  // js.configs.recommended,
+  ...pluginVue.configs["flat/recommended"],
+  // ...pluginVue.configs['flat/vue2-recommended'], // Use this if you are using Vue.js 2.x.
+  {
+    rules: {
+      // override/add rules settings here, such as:
+      // 'vue/no-unused-vars': 'error'
+    },
+    languageOptions: {
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+      },
+    },
+  },
+]);
