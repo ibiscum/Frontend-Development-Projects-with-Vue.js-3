91b4811aefaf0d7c2440d79bb34e692a0ea425bf
ibiscum <ulf.luecking@gmail.com>
2025-09-05 00:25:19 +0200

    update

--- Chapter04/Exercise4.01/src/App.vue
+++ Chapter04/Exercise4.01/src/App.vue
@@ -3,6 +3,9 @@ import Greeting from './components/Greeting.vue'
 </script>
 <template>
   <div id="app">
-    <Greeting greeting="Hi" who="everyone"/>
+    <Greeting 
+      greeting="Hi" 
+      who="everyone"
+    />  
   </div>
 </template>
