59e2819871815e11a1e56ca4975b3e4eb1edf632
ibiscum <ulf.luecking@gmail.com>
2025-09-12 00:15:32 +0200

    ch14

--- Chapter04/Exercise4.01/src/components/GreetingComp.vue
+++ Chapter04/Exercise4.01/src/components/GreetingComp.vue
@@ -1,8 +1,12 @@
 <template>
   <div>{{ greeting }} {{ who }}</div>
 </template>
+
 <script>
 export default {
-  props: {greeting: {type: String, default: 'greeting'}, who: {type: String, default: 'who'} },
+  props: {
+    greeting: {type: String, default: 'greeting'},
+    who: {type: String, default: 'who'}
+  }
 };
 </script>
