ba76305f853b7227dafbd6741c0127bfa1fa7eeb
ibiscum <ulf.luecking@gmail.com>
2026-01-02 17:04:06 +0100

    ch10

--- chapter-10/exercise10.02/src/stores/color.js
+++ chapter-10/exercise10.02/src/stores/color.js
@@ -1,7 +1,6 @@
 import { defineStore } from "pinia";
 
-export const useColorStore = defineStore({
-  id: "color",
+export const useColorStore = defineStore('color', {
   state: () => ({
     red: 0,
     blue: 0,
