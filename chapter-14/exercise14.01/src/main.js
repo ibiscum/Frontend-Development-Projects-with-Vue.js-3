60f52687366a910ca7635943342d4bac7ec6c7af
ibiscum <ulf.luecking@gmail.com>
2026-01-11 12:10:19 +0100

    ch14

--- chapter-14/exercise14.01/src/main.js
+++ chapter-14/exercise14.01/src/main.js
@@ -1,9 +1,14 @@
-import Vue from "vue";
-import "@/assets/css/tailwind.css";
+// import Vue from "vue";
+// import "./assets/css/tailwind.css";
+// import App from "./App.vue";
+
+// Vue.config.productionTip = false;
+
+// new Vue({
+//   render: (h) => h(App),
+// }).$mount("#app");
+
+import { createApp } from "vue";
 import App from "./App.vue";
 
-Vue.config.productionTip = false;
-
-new Vue({
-  render: (h) => h(App),
-}).$mount("#app");
+createApp(App).mount("#app");
